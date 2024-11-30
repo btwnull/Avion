30fdb86076e0923d304e30c229c6cf6e87cab91c
Ivan Sobenin <dn.sobenin75@gmail.com>
2024-11-30 23:37:46 +0300

    сделал страницу About

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const noticeEl = document.querySelector(".notice");
+if (noticeEl) {
+  const noticeCloseEl = noticeEl.querySelector(".notice__close");
+  noticeCloseEl.addEventListener("click", () => {
+    noticeEl.classList.add("notice--hidden");
+  });
+}
